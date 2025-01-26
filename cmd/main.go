56895ae199a18c0dfd65a65a6cbfd5c5d3fa94f7
Rueian <rueiancsie@gmail.com>
2025-01-25 22:46:09 -0800

    chore: refactor
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -20,86 +19,13 @@ import (
 
 func main() {
 	log.SetFlags(0)
-
 	var entryCmd = &cobra.Command{
 		Use: "git-lfs-fuse",
 	}
-
 	var mountCmd = &cobra.Command{
+		Run:   mountRun,
 		Use:   "mount [<options>] [--] <repo> [<dir>]",
 		Short: "Mount the provided repository to the local directory",
-		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return errors.New("you must specify a repository to clone")
-			}
-			return nil
-		},
-		Run: func(cmd *cobra.Command, args []string) {
-			dst := strings.TrimSuffix(filepath.Base(args[0]), ".git")
-			dir, err := filepath.Abs(".")
-			if len(args) >= 2 {
-				dst = filepath.Base(args[1])
-				dir, err = filepath.Abs(filepath.Dir(args[1]))
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			hid := filepath.Join(dir, "."+dst)
-			mnt := filepath.Join(dir, dst)
-
-			git := exec.Command("git", "clone")
-			cmd.Flags().Visit(func(flg *pflag.Flag) {
-				switch flg.Name {
-				case "origin", "branch", "depth":
-					git.Args = append(git.Args, fmt.Sprintf("--%s=%s", flg.Name, flg.Value.String()))
-				default:
-					git.Args = append(git.Args, fmt.Sprintf("--%s", flg.Name))
-				}
-			})
-			git.Args = append(git.Args, "--", args[0], hid)
-			git.Stdout = os.Stdout
-			git.Stderr = os.Stderr
-			git.Env = os.Environ()
-			git.Env = append(git.Env, "GIT_LFS_SKIP_SMUDGE=1")
-			if err := git.Run(); err != nil {
-				log.Fatal(err)
-			}
-
-			cfg := config.NewIn(hid, "")
-			lfo := lfs.FilterOptions{
-				GitConfig:  cfg.GitConfig(),
-				Force:      true,
-				Local:      true,
-				SkipSmudge: true,
-			}
-			if err := lfo.Install(); err != nil {
-				log.Fatal(err)
-			}
-
-			pxy, err := fs.NewLoopbackRoot(hid)
-			if err != nil {
-				log.Fatal(err)
-			}
-			svc, err := fs.Mount(mnt, pxy, &fs.Options{
-				MountOptions: fuse.MountOptions{
-					FsName: dst,
-					Name:   dst,
-				},
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s is mounted at %s. Please keep this process running.", args[0], mnt)
-
-			sig := make(chan os.Signal, 1)
-			signal.Notify(sig, os.Interrupt)
-			go func() {
-				for err := os.ErrInvalid; err != nil; err = svc.Unmount() {
-					<-sig
-				}
-			}()
-			svc.Wait()
-		},
 	}
 	mountCmd.Flags().Bool("progress", false, "force progress reporting")
 	mountCmd.Flags().BoolP("no-checkout", "n", false, "don't clone shallow repository")
@@ -118,4 +44,74 @@ func main() {
 	if err := entryCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func mountRun(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		log.Fatal("You must specify a repository to clone")
+	}
+
+	dst := strings.TrimSuffix(filepath.Base(args[0]), ".git")
+	dir, err := filepath.Abs(".")
+	if len(args) >= 2 {
+		dst = filepath.Base(args[1])
+		dir, err = filepath.Abs(filepath.Dir(args[1]))
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	hid := filepath.Join(dir, "."+dst)
+	mnt := filepath.Join(dir, dst)
+
+	git := exec.Command("git", "clone")
+	cmd.Flags().Visit(func(flg *pflag.Flag) {
+		switch flg.Name {
+		case "origin", "branch", "depth":
+			git.Args = append(git.Args, fmt.Sprintf("--%s=%s", flg.Name, flg.Value.String()))
+		default:
+			git.Args = append(git.Args, fmt.Sprintf("--%s", flg.Name))
+		}
+	})
+	git.Args = append(git.Args, "--", args[0], hid)
+	git.Stdout = os.Stdout
+	git.Stderr = os.Stderr
+	git.Env = os.Environ()
+	git.Env = append(git.Env, "GIT_LFS_SKIP_SMUDGE=1")
+	if err := git.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	lfo := lfs.FilterOptions{
+		GitConfig:  config.NewIn(hid, "").GitConfig(),
+		Force:      true,
+		Local:      true,
+		SkipSmudge: true,
+	}
+	if err := lfo.Install(); err != nil {
+		log.Fatal(err)
+	}
+
+	pxy, err := fs.NewLoopbackRoot(hid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	svc, err := fs.Mount(mnt, pxy, &fs.Options{
+		MountOptions: fuse.MountOptions{
+			FsName: dst,
+			Name:   dst,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%s is mounted at %s.\nPlease keep this process running.", args[0], mnt)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	go func() {
+		for err := os.ErrInvalid; err != nil; err = svc.Unmount() {
+			<-sig
+		}
+	}()
+	svc.Wait()
 }
