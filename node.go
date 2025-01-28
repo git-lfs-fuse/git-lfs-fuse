b700acfb15906251b6e1e8394b539516e4caee8f
Rueian <rueiancsie@gmail.com>
2025-01-27 22:48:59 -0800

    chore: refactor
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- node.go
+++ node.go
@@ -35,26 +35,29 @@ type FSNodeData struct {
 }
 
 var _ = (fs.NodeStatfser)((*FSNode)(nil))
+var _ = (fs.NodeLookuper)((*FSNode)(nil))
+var _ = (fs.NodeMknoder)((*FSNode)(nil))
+var _ = (fs.NodeMkdirer)((*FSNode)(nil))
+var _ = (fs.NodeRmdirer)((*FSNode)(nil))
+var _ = (fs.NodeUnlinker)((*FSNode)(nil))
+var _ = (fs.NodeRenamer)((*FSNode)(nil))
+var _ = (fs.NodeCreater)((*FSNode)(nil))
+var _ = (fs.NodeSymlinker)((*FSNode)(nil))
+var _ = (fs.NodeLinker)((*FSNode)(nil))
+var _ = (fs.NodeReadlinker)((*FSNode)(nil))
+var _ = (fs.NodeOpener)((*FSNode)(nil))
+var _ = (fs.NodeReader)((*FSNode)(nil))
+var _ = (fs.NodeOpendirHandler)((*FSNode)(nil))
+var _ = (fs.NodeReaddirer)((*FSNode)(nil))
+var _ = (fs.NodeGetattrer)((*FSNode)(nil))
+var _ = (fs.NodeSetattrer)((*FSNode)(nil))
+var _ = (fs.NodeGetxattrer)((*FSNode)(nil))
+var _ = (fs.NodeSetxattrer)((*FSNode)(nil))
+var _ = (fs.NodeRemovexattrer)((*FSNode)(nil))
+var _ = (fs.NodeCopyFileRanger)((*FSNode)(nil))
 
 func (n *FSNode) Statfs(ctx context.Context, out *fuse.StatfsOut) syscall.Errno {
 	return n.LoopbackNode.Statfs(ctx, out)
-}
-
-var _ = (fs.NodeLookuper)((*FSNode)(nil))
-
-func (n *FSNode) fixAttr(out *fuse.Attr, path string) {
-	if out.Size >= 1024 {
-		return
-	}
-	if r, err := os.Open(path); err == nil {
-		if ptr, _ := lfs.DecodePointer(r); ptr != nil {
-			out.Size = uint64(ptr.Size)
-			if out.Blksize != 0 {
-				out.Blocks = (out.Size + uint64(out.Blksize) - 1) / uint64(out.Blksize)
-			}
-		}
-		_ = r.Close()
-	}
 }
 
 func (n *FSNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
@@ -64,8 +67,6 @@ func (n *FSNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*
 	return n.LoopbackNode.Lookup(ctx, name, out)
 }
 
-var _ = (fs.NodeMknoder)((*FSNode)(nil))
-
 func (n *FSNode) Mknod(ctx context.Context, name string, mode, rdev uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	if !n.Metadata.(*FSNodeData).Ignore {
 		defer n.fixAttr(&out.Attr, filepath.Join(n.path(), name))
@@ -73,65 +74,36 @@ func (n *FSNode) Mknod(ctx context.Context, name string, mode, rdev uint32, out 
 	return n.LoopbackNode.Mknod(ctx, name, mode, rdev, out)
 }
 
-var _ = (fs.NodeMkdirer)((*FSNode)(nil))
-
 func (n *FSNode) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	return n.LoopbackNode.Mkdir(ctx, name, mode, out)
 }
-
-var _ = (fs.NodeRmdirer)((*FSNode)(nil))
 
 func (n *FSNode) Rmdir(ctx context.Context, name string) syscall.Errno {
 	return n.LoopbackNode.Rmdir(ctx, name)
 }
 
-var _ = (fs.NodeUnlinker)((*FSNode)(nil))
-
 func (n *FSNode) Unlink(ctx context.Context, name string) syscall.Errno {
 	return n.LoopbackNode.Unlink(ctx, name)
 }
-
-var _ = (fs.NodeRenamer)((*FSNode)(nil))
 
 func (n *FSNode) Rename(ctx context.Context, name string, newParent fs.InodeEmbedder, newName string, flags uint32) syscall.Errno {
 	return n.LoopbackNode.Rename(ctx, name, newParent, newName, flags)
 }
 
-var _ = (fs.NodeCreater)((*FSNode)(nil))
-
 func (n *FSNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (inode *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
 	return n.LoopbackNode.Create(ctx, name, flags, mode, out)
 }
-
-var _ = (fs.NodeSymlinker)((*FSNode)(nil))
 
 func (n *FSNode) Symlink(ctx context.Context, target, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	return n.LoopbackNode.Symlink(ctx, target, name, out)
 }
 
-var _ = (fs.NodeLinker)((*FSNode)(nil))
-
 func (n *FSNode) Link(ctx context.Context, target fs.InodeEmbedder, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	return n.LoopbackNode.Link(ctx, target, name, out)
 }
 
-var _ = (fs.NodeReadlinker)((*FSNode)(nil))
-
 func (n *FSNode) Readlink(ctx context.Context) ([]byte, syscall.Errno) {
 	return n.LoopbackNode.Readlink(ctx)
-}
-
-var _ = (fs.NodeOpener)((*FSNode)(nil))
-
-func (n *FSNode) root() *fs.Inode {
-	if n.RootData.RootNode != nil {
-		return n.RootData.RootNode.EmbeddedInode()
-	}
-	return n.Root()
-}
-
-func (n *FSNode) path() string {
-	return filepath.Join(n.RootData.Path, n.Path(n.root()))
 }
 
 func (n *FSNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
@@ -168,8 +140,6 @@ func (n *FSNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuse
 	return lf, 0, 0
 }
 
-var _ = (fs.NodeReader)((*FSNode)(nil))
-
 func (n *FSNode) Read(ctx context.Context, f fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
 	if f == nil {
 		return nil, fs.ToErrno(errors.New("f is nil " + n.path()))
@@ -180,19 +150,13 @@ func (n *FSNode) Read(ctx context.Context, f fs.FileHandle, dest []byte, off int
 	return nil, fs.ToErrno(errors.New("f is not a FileReader " + n.path()))
 }
 
-var _ = (fs.NodeOpendirHandler)((*FSNode)(nil))
-
 func (n *FSNode) OpendirHandle(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
 	return n.LoopbackNode.OpendirHandle(ctx, flags)
 }
 
-var _ = (fs.NodeReaddirer)((*FSNode)(nil))
-
 func (n *FSNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
 	return n.LoopbackNode.Readdir(ctx)
 }
-
-var _ = (fs.NodeGetattrer)((*FSNode)(nil))
 
 func (n *FSNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	if f == nil && !n.Metadata.(*FSNodeData).Ignore {
@@ -205,8 +169,6 @@ func (n *FSNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut
 	return n.LoopbackNode.Getattr(ctx, f, out)
 }
 
-var _ = (fs.NodeSetattrer)((*FSNode)(nil))
-
 func (n *FSNode) Setattr(ctx context.Context, f fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
 	if f == nil && !n.Metadata.(*FSNodeData).Ignore {
 		defer n.fixAttr(&out.Attr, n.path())
@@ -218,30 +180,48 @@ func (n *FSNode) Setattr(ctx context.Context, f fs.FileHandle, in *fuse.SetAttrI
 	return n.LoopbackNode.Setattr(ctx, f, in, out)
 }
 
-var _ = (fs.NodeGetxattrer)((*FSNode)(nil))
-
 func (n *FSNode) Getxattr(ctx context.Context, attr string, dest []byte) (uint32, syscall.Errno) {
 	return n.LoopbackNode.Getxattr(ctx, attr, dest)
 }
-
-var _ = (fs.NodeSetxattrer)((*FSNode)(nil))
 
 func (n *FSNode) Setxattr(ctx context.Context, attr string, data []byte, flags uint32) syscall.Errno {
 	return n.LoopbackNode.Setxattr(ctx, attr, data, flags)
 }
 
-var _ = (fs.NodeRemovexattrer)((*FSNode)(nil))
-
 func (n *FSNode) Removexattr(ctx context.Context, attr string) syscall.Errno {
 	return n.LoopbackNode.Removexattr(ctx, attr)
 }
-
-var _ = (fs.NodeCopyFileRanger)((*FSNode)(nil))
 
 func (n *FSNode) CopyFileRange(ctx context.Context, fhIn fs.FileHandle,
 	offIn uint64, out *fs.Inode, fhOut fs.FileHandle, offOut uint64,
 	len uint64, flags uint64) (uint32, syscall.Errno) {
 	return n.LoopbackNode.CopyFileRange(ctx, fhIn, offIn, out, fhOut, offOut, len, flags)
+}
+
+func (n *FSNode) fixAttr(out *fuse.Attr, path string) {
+	if out.Size >= 1024 {
+		return
+	}
+	if r, err := os.Open(path); err == nil {
+		if ptr, _ := lfs.DecodePointer(r); ptr != nil {
+			out.Size = uint64(ptr.Size)
+			if out.Blksize != 0 {
+				out.Blocks = (out.Size + uint64(out.Blksize) - 1) / uint64(out.Blksize)
+			}
+		}
+		_ = r.Close()
+	}
+}
+
+func (n *FSNode) root() *fs.Inode {
+	if n.RootData.RootNode != nil {
+		return n.RootData.RootNode.EmbeddedInode()
+	}
+	return n.Root()
+}
+
+func (n *FSNode) path() string {
+	return filepath.Join(n.RootData.Path, n.Path(n.root()))
 }
 
 const chunksize = 4 * 1024 * 1024
