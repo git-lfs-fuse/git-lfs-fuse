16e03750ce9307fd483c6d4d19db53e7251fc639
Rueian <rueiancsie@gmail.com>
2025-04-19 13:44:27 -0700

    test: lfs errors
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- page_test.go
+++ page_test.go
@@ -1,12 +1,119 @@
 package gitlfsfuse
 
 import (
+	"errors"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"sync/atomic"
+	"syscall"
 	"testing"
 	"time"
 )
+
+func TestPageFetcher_Error_503(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			w.WriteHeader(http.StatusServiceUnavailable)
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Error_404_1(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			w.WriteHeader(http.StatusNotFound)
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Error_404_2(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{
+			  "operation": "download",
+			  "transfers": [ "basic" ],
+			  "ref": { "name": "refs/heads/main" },
+			  "objects": [],
+			  "hash_algo": "sha256"
+			}`))
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Error_404_3(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{
+				"operation": "download",
+				"transfers": [ "basic" ],
+				"ref": { "name": "refs/heads/main" },
+				"objects": [
+					{
+						"error": {
+							"code": 404,
+							"message": "Object does not exist"
+						}
+					}
+				],
+				"hash_algo": "sha256"
+			}`))
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Error_Missing_Link(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{
+				"operation": "download",
+				"transfers": [ "basic" ],
+				"ref": { "name": "refs/heads/main" },
+				"objects": [
+					{}
+				],
+				"hash_algo": "sha256"
+			}`))
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
 
 func TestPageFetcher_FetchRetries(t *testing.T) {
 	var retries int64
@@ -34,4 +141,60 @@ func TestPageFetcher_FetchRetries(t *testing.T) {
 	defer cancel()
 
 	_ = verifyRemoteFile(t, hid, mnt, "emptylarge.bin")
+}
+
+func TestPageFetcher_Fetch_WrongStatus(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			if r.Method == http.MethodGet && strings.HasPrefix(r.URL.Path, "/objects") {
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+			handler.ServeHTTP(w, r)
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Fetch_WrongRange(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			if r.Method == http.MethodGet && strings.HasPrefix(r.URL.Path, "/objects") {
+				w.Header().Set("Content-Range", "bytes 0000000")
+				w.WriteHeader(http.StatusPartialContent)
+				return
+			}
+			handler.ServeHTTP(w, r)
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
+}
+
+func TestPageFetcher_Fetch_WrongRange_2(t *testing.T) {
+	_, mnt, cancel := cloneMountWithCacheSize(t, 100, func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("%s %s", r.Method, r.URL)
+			if r.Method == http.MethodGet && strings.HasPrefix(r.URL.Path, "/objects") {
+				w.Header().Set("Content-Range", "bytes 1111111-222222/2309409234")
+				w.WriteHeader(http.StatusPartialContent)
+				return
+			}
+			handler.ServeHTTP(w, r)
+		})
+	})
+	defer cancel()
+
+	if _, err := os.ReadFile(filepath.Join(mnt, "emptylarge.bin")); !errors.Is(err, syscall.EBADF) {
+		t.Fatal(err)
+	}
 }
